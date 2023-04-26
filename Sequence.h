38531f77b4afccff51b51fac1b05ca32b1888b8d
AverageComputerNerd <130625952+AverageComputerNerd@users.noreply.github.com>
2023-04-25 22:53:01 -0700

    Create Sequence.h

---
+++ Sequence.h
@@ -0,0 +1,41 @@
+#ifndef Sequence_h  //include guards
+#define Sequence_h
+#include <string>
+
+using ItemType = std::string;
+
+class Sequence
+{
+public:
+    Sequence();
+    Sequence(const Sequence& other);  //copy constructor
+    Sequence& operator = (const Sequence& other); //assignment operator
+    ~Sequence();  //destrucotor
+    
+    bool empty() const;
+    int size() const;
+    int insert (int pos, const ItemType& value);
+    int insert (const ItemType& value);
+    bool erase (int pos);
+    int remove (const ItemType& value);
+    bool get (int pos, ItemType& value) const;
+    bool set (int pos, const ItemType& value);
+    int find (const ItemType& value) const;
+    void swap (Sequence& other);
+    
+private:
+    struct Node
+    {
+        ItemType data;
+        Node* next;
+        Node* previous;
+    };
+    Node* head;
+    Node* tail;
+    int m_size;
+};
+
+int subsequence(const Sequence& seq1, const Sequence& seq2);
+void concatReverse(const Sequence& seq1, const Sequence& seq2, Sequence& result);
+
+#endif /*Sequence_h*/
